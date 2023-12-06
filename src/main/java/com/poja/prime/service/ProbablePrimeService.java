f3f80d9dd386b47c42a35ece270b71845b416234
amourRamanantsiresy <hei.amour.2@gmail.com>
2023-12-06 09:43:49 +0300

    chore: change MAX_PRIME_BIT to 10

--- src/main/java/com/poja/prime/service/ProbablePrimeService.java
+++ src/main/java/com/poja/prime/service/ProbablePrimeService.java
@@ -13,7 +13,7 @@ import java.util.Random;
 public class ProbablePrimeService {
     private HistoryService historyService;
     public BigInteger getProbablePrime(){
-        int MAX_PRIME_BIT = 10_000;
+        int MAX_PRIME_BIT = 10;
         int CERTAINLY = 1;
         BigInteger generatedPrime =  new BigInteger(MAX_PRIME_BIT, CERTAINLY,new Random());
         History currentHistory = History.builder().value(generatedPrime.toString()).build();
